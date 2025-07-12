743a90565c71f0c99779cfc14aefebb9873839b2
Tshepiso <tlhongtshepiso2@gmail.com>
2025-07-12 03:12:12 +0200

    faces.py

--- Week_0/faces.py
+++ Week_0/faces.py
@@ -1,5 +1,4 @@
 
-
 def main():
     user_input = input("Say something with am emotion: \n")
     output = convert_to_emoji(user_input)
@@ -10,6 +9,11 @@ def convert_to_emoji(user_input):
     if ":)" in user_input:
         converted = user_input.replace(":)", "😊")
         return converted
+    elif ":(" in user_input:
+        converted = user_input.replace(":(","🙁")
+        return converted
+    else:
+        return user_input
 
 
 if __name__ == "__main__":
