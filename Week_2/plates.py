95282d852a27333944a527083b90c81fa4f5a880
Tshepiso <tlhongtshepiso2@gmail.com>
2025-07-18 17:21:40 +0200

    plates not complete yet

---
+++ Week_2/plates.py
@@ -0,0 +1,18 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    lttrs = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    if s[0:1] in lttrs:
+        return True   
+    if s > 2 and s < 6:
+        ...
+
+print("Hello")
+if __name__ == "__main__":
+    main()  
