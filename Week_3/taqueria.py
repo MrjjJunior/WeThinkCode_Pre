0d5c5a46ad894091e7723e81e1f5d4f52912bd49
Tshepiso <tlhongtshepiso2@gmail.com>
2025-07-22 20:23:26 +0200

    updates

---
+++ Week_3/taqueria.py
@@ -0,0 +1,22 @@
+def main():
+    price_list = {
+        "Baja Taco": 4.25,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00
+        }
+    for item in price_list:
+        print(item, price_list[item])
+
+
+def shop():
+    ...
+
+
+if __name__ == "__main__":
+    main()
