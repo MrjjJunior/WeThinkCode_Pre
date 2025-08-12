5b9b0e6bae06b37f8abd63b7862fda4440c94fb9
Tshepiso <tlhongtshepiso2@gmail.com>
2025-08-12 21:25:43 +0200

    fixing lines.py

--- Week_6/lines.py
+++ Week_6/lines.py
@@ -1,15 +1,25 @@
 import sys
+
 """ script takes a file as an argument and counts lines of code """
+
 
 def main():
     code = []
-    with open(sys.argv[1]) as file:
-        for line in file:
-            if line.rstrip() == "" or line.startswith("#"):
-                continue
-            else:
-                code.append(line.rstrip())
-    print(len(code))
+
+    try:
+        if len(sys.argv) < 2:
+            sys.exit("Too few command-line arguments")
+        elif len(sys.argv) > 2:
+            sys.exit("Too many command-line arguments")
+        with open(sys.argv[1]) as file:
+            for line in file:
+                if line.rstrip() == "" or line.startswith("#"):
+                    continue
+                else:
+                    code.append(line.rstrip())
+        print(len(code))
+    except FileNotFoundError:
+        print("File does not exist")
 
 
 if __name__ == "__main__":
